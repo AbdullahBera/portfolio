fb338ba6f799fed5ec96651e674e7c28426b4b57
AbdullahBera <kucukabdullahbera@gmail.com>
2024-09-01 01:25:24 -0700

    contact commit

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -8,13 +8,13 @@ export default function Contact() {
   return (
     <section className={"max-w-3xl px-5 py-8 mx-auto"}>
       <div className="flex gap-x-4">
-        <a href="https://github.com/abdullahberan" target="_blank">
+        <a href="https://github.com/AbdullahBera" target="_blank">
           <GitHubLogoIcon className="w-6 h-6 text-gray-600 hover:text-gray-800 transition-all hover:cursor-pointer hover:rotate-3" />
         </a>
-        <a href=" https://twitter.com/abdullahberan" target="_blank">
+        <a href="https://x.com/aberakucuk" target="_blank">
           <TwitterLogoIcon className="w-6 h-6 text-gray-600 hover:text-gray-800 transition-all hover:cursor-pointer hover:rotate-3" />
         </a>
-        <a href="https://www.linkedin.com/in/abdullahbera/" target="_blank">
+        <a href="https://www.linkedin.com/in/abdullahberakucuk/" target="_blank">
           <LinkedInLogoIcon className="w-6 h-6 text-gray-600 hover:text-gray-800 transition-all hover:cursor-pointer hover:rotate-3" />
         </a>
       </div>
